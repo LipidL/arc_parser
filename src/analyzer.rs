9d63beac32b23c7fea7e5162ba5a6682d096fc63
lipid <lipid.l@qq.com>
2023-11-20 21:02:02 +0800

    version0 completed 完成找极小 TODO: 命令行解析；统计结构数

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -1,7 +1,6 @@
 pub mod arc_analyzer{
     use crate::modules::structures:: StructureBlock;
     pub fn find_minimum_energy(blocks: Vec<StructureBlock>) -> Option<f64> {
-    println!("Hello, world!");
         blocks.iter().fold(None, |min, b| match min {
             None => Some(b.energy),
             Some(min_energy) => Some(min_energy.min(b.energy)),
--- src/parser.rs
+++ src/parser.rs
@@ -6,7 +6,7 @@ pub mod arc_parser{
     use regex::Regex;
 
     fn parse_block_header(input: &str) -> Option<(i32, f64, f64, String)> {
-        let re = Regex::new(r"^Energy\s+(\d+)\s+([0-9.]+)\s+([0-9.]+)\s+(.*)$").unwrap();
+        let re = Regex::new(r"^\s+Energy\s+(\d+)\s+([0-9.]+)\s+(-?[0-9.]+)\s+(.*)$").unwrap();
         if let Some(captures) = re.captures(input) {
             let number = captures[1].parse::<i32>().unwrap();
             let float1 = captures[2].parse::<f64>().unwrap();
@@ -19,7 +19,8 @@ pub mod arc_parser{
     }
 
     fn parse_atom_data(input: &str) -> Option<(String, f64, f64, f64)> {
-        let re = Regex::new(r"(?P<s>\w+)\s+(?P<f1>\d+\.\d+)\s+(?P<f2>\d+\.\d+)\s+(?P<f3>\d+\.\d+)").unwrap();
+        //^(\w+)\s+(-?\d+\.\d+)\s+(-?\d+\.\d+)\s+(-?\d+\.\d+)\s+.*
+        let re = Regex::new(r"^(?P<s>\w+)\s+(?P<f1>-?\d+\.\d+)\s+(?P<f2>-?\d+\.\d+)\s+(?P<f3>-?\d+\.\d+)\s+CORE\s+.*").unwrap();
         if let Some(caps) = re.captures(input) {
             let s = caps.name("s").unwrap().as_str().to_string();
             let f1 = caps.name("f1").unwrap().as_str().parse().unwrap();
