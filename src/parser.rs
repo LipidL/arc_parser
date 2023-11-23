b6bdde974de1eae1a0113ba07e35fa683c460465
lipid <lipid.l@qq.com>
2023-11-23 09:19:59 +0800

    encapsulate regex functions improved performance & reduced coupling

--- src/parser.rs
+++ src/parser.rs
@@ -7,98 +7,113 @@ pub mod arc_parser{
     use regex::Regex;
 
     /**
-    match the block header and extract necessary values.
-     
-    common header:
-        Energy         0          0.0099      -3620.679360        C1
-
-    extracts: 
-     + number: u64, serial number of the block, 0 in this case
-     + float1: f64, a float that I don't know what it means
-     + energy: f64, the energy(eV) of this block, -3620.679360 in this case
-     + symmetry: String, symmetry of this block, C1 in this case
-
-     *NOTE!!!*
-
-     parameter re should be
-     `Regex::new(r"^\s+Energy\s+(\d+)\s+([0-9.]+)\s+(-?[0-9.]+)\s+(.*)$").unwrap()`
-     place it elsewhere to improve performance
-    */     
-    fn parse_block_header(input: &str, re: &Regex) -> Option<(u64, f64, f64, String)> {
-
-        if let Some(captures) = re.captures(input) {
-            let number = captures[1].parse::<u64>().unwrap();
-            let float1 = captures[2].parse::<f64>().unwrap();
-            let energy = captures[3].parse::<f64>().unwrap();
-            let symmetry = captures[4].to_string();
-            Some((number, float1, energy, symmetry))
-        } else {
+    an encapsulation of regex function parse_atom_data
+     */
+    pub struct AtomDataParser{
+        re: Regex,
+    }
+    impl AtomDataParser{
+        pub fn new() -> Self{
+            let atom_data_regex = Regex::new(r"^(?P<s>\w+)\s+(?P<f1>-?\d+\.\d+)\s+(?P<f2>-?\d+\.\d+)\s+(?P<f3>-?\d+\.\d+)\s+CORE\s+.*").unwrap();
+            Self { re: atom_data_regex }
+        }
+        /**
+        match the atom information and extract necessary values.
+        
+        common atom info:
+            C        7.210469000   10.148070000    0.813536200 CORE    1 C  C    0.0000    1
+        extracts:
+        + s: String,  element
+        + f1: f64, x value
+        + f2: f64, y value
+        + f3: f64, z value
+        */
+        pub fn parse_atom_data(&self, input: &str) -> Option<(String, f64, f64, f64)> {
+            if let Some(caps) = self.re.captures(input) {
+                let s = caps.name("s").unwrap().as_str().to_string();
+                let f1 = caps.name("f1").unwrap().as_str().parse().unwrap();
+                let f2 = caps.name("f2").unwrap().as_str().parse().unwrap();
+                let f3 = caps.name("f3").unwrap().as_str().parse().unwrap();
+                return Some((s, f1, f2, f3));
+            }
             None
         }
     }
 
     /**
-    match the atom information and extract necessary values.
-     
-    common atom info:
-        C        7.210469000   10.148070000    0.813536200 CORE    1 C  C    0.0000    1
-    extracts:
-    + s: String,  element
-    + f1: f64, x value
-    + f2: f64, y value
-    + f3: f64, z value
-    
-    *NOTE!!!*
-    
-    parameter re should be
-    `Regex::new(r"^(?P<s>\w+)\s+(?P<f1>-?\d+\.\d+)\s+(?P<f2>-?\d+\.\d+)\s+(?P<f3>-?\d+\.\d+)\s+CORE\s+.*").unwrap()`
-    place it elsewhere to improve performance
+    an encapsulation fo regex function parse_block_header
      */
-    fn parse_atom_data(input: &str, re: &Regex) -> Option<(String, f64, f64, f64)> {
-        //let re = Regex::new(r"^(?P<s>\w+)\s+(?P<f1>-?\d+\.\d+)\s+(?P<f2>-?\d+\.\d+)\s+(?P<f3>-?\d+\.\d+)\s+CORE\s+.*").unwrap();
-        if let Some(caps) = re.captures(input) {
-            let s = caps.name("s").unwrap().as_str().to_string();
-            let f1 = caps.name("f1").unwrap().as_str().parse().unwrap();
-            let f2 = caps.name("f2").unwrap().as_str().parse().unwrap();
-            let f3 = caps.name("f3").unwrap().as_str().parse().unwrap();
-            return Some((s, f1, f2, f3));
+    pub struct BlockHeaderParser{
+        re: Regex,
+    }
+    impl BlockHeaderParser{
+        pub fn new() -> Self{
+            let block_header_regex = Regex::new(r"^\s+Energy\s+(\d+)\s+([0-9.]+)\s+(-?[0-9.]+)\s+(.*)$").unwrap();
+            Self { re: block_header_regex }
         }
-        None
+        /**
+        match the block header and extract necessary values.
+        
+        common header:
+            Energy         0          0.0099      -3620.679360        C1
+
+        extracts: 
+        + number: u64, serial number of the block, 0 in this case
+        + float1: f64, a float that I don't know what it means
+        + energy: f64, the energy(eV) of this block, -3620.679360 in this case
+        + symmetry: String, symmetry of this block, C1 in this case
+        */     
+        pub fn parse_block_header(&self, input: &str) -> Option<(u64, f64, f64, String)> {
+
+            if let Some(captures) = self.re.captures(input) {
+                let number = captures[1].parse::<u64>().unwrap();
+                let float1 = captures[2].parse::<f64>().unwrap();
+                let energy = captures[3].parse::<f64>().unwrap();
+                let symmetry = captures[4].to_string();
+                Some((number, float1, energy, symmetry))
+            } else {
+                None
+            }
+        } 
     }
 
-    /**
-    match cell information and extract necessary values.
-    
-    common cell information header:
-    PBC   20.19500000   20.19500000   29.51410000   90.00000000   90.00000000  120.00000000
-    extracts:
-    + crystal: CrystalInfo, cell information of the block
-    
-    *NOTE!!!*
-    
-    parameter re should be 
-    `Regex::new(r"^\w+\s+(?P<x>\d+\.\d+)\s+(?P<y>\d+\.\d+)\s+(?P<z>\d+\.\d+)\s+(?P<alpha>\d+.\d+)\s+(?P<beta>\d+.\d+)\s+(?P<gamma>\d+.\d+)").unwrap()`
-    place it elsewhere to improve performance
-    */
-    fn parse_crystal_data(input: &str, re: &Regex) -> Option<CrystalInfo>{
-        if let Some(caps) = re.captures(input) {
-            let x = caps.name("x").unwrap().as_str().parse().unwrap();
-            let y = caps.name("y").unwrap().as_str().parse().unwrap();
-            let z = caps.name("z").unwrap().as_str().parse().unwrap();
-            let alpha = caps.name("alpha").unwrap().as_str().parse().unwrap();
-            let beta = caps.name("beta").unwrap().as_str().parse().unwrap();
-            let gamma = caps.name("gamma").unwrap().as_str().parse().unwrap();
-            let crystal = CrystalInfo{
-                x,
-                y,
-                z,
-                alpha,
-                beta,
-                gamma
-            };
-            return Some(crystal);
+
+    pub struct CellDataParser{
+        re: Regex,
+    }
+    impl CellDataParser{
+        fn new() -> Self{
+            let cell_info_regex = Regex::new(r"^\w+\s+(?P<x>\d+\.\d+)\s+(?P<y>\d+\.\d+)\s+(?P<z>\d+\.\d+)\s+(?P<alpha>\d+.\d+)\s+(?P<beta>\d+.\d+)\s+(?P<gamma>\d+.\d+)").unwrap();
+            Self { re: cell_info_regex }
         }
-        None     
+        /**
+        match cell information and extract necessary values.
+        
+        common cell information header:
+        PBC   20.19500000   20.19500000   29.51410000   90.00000000   90.00000000  120.00000000
+        extracts:
+        + crystal: CrystalInfo, cell information of the block
+        */
+        fn parse_cell_info(&self, input: &str) -> Option<CrystalInfo>{
+            if let Some(caps) = self.re.captures(input) {
+                let x = caps.name("x").unwrap().as_str().parse().unwrap();
+                let y = caps.name("y").unwrap().as_str().parse().unwrap();
+                let z = caps.name("z").unwrap().as_str().parse().unwrap();
+                let alpha = caps.name("alpha").unwrap().as_str().parse().unwrap();
+                let beta = caps.name("beta").unwrap().as_str().parse().unwrap();
+                let gamma = caps.name("gamma").unwrap().as_str().parse().unwrap();
+                let crystal = CrystalInfo{
+                    x,
+                    y,
+                    z,
+                    alpha,
+                    beta,
+                    gamma
+                };
+                return Some(crystal);
+            }
+            None     
+        }
     }
 
     /**
@@ -114,15 +129,15 @@ pub mod arc_parser{
         let mut blocks:Vec<StructureBlock> = Vec::new();
         let mut current_block: Option<StructureBlock> = None;
         //compile necessary regex
-        let block_header_regex = Regex::new(r"^\s+Energy\s+(\d+)\s+([0-9.]+)\s+(-?[0-9.]+)\s+(.*)$").unwrap();
-        let atom_data_regex = Regex::new(r"^(?P<s>\w+)\s+(?P<f1>-?\d+\.\d+)\s+(?P<f2>-?\d+\.\d+)\s+(?P<f3>-?\d+\.\d+)\s+CORE\s+.*").unwrap();
-        let crystal_info_regex = Regex::new(r"^\w+\s+(?P<x>\d+\.\d+)\s+(?P<y>\d+\.\d+)\s+(?P<z>\d+\.\d+)\s+(?P<alpha>\d+.\d+)\s+(?P<beta>\d+.\d+)\s+(?P<gamma>\d+.\d+)").unwrap();
+        let block_header_parser = BlockHeaderParser::new();
+        let atom_data_parser = AtomDataParser::new();
+        let cell_data_parser = CellDataParser::new();
         //read the file line by line
         for line in reader.lines(){
             //handle cases of io error
             let line = line?;
             //check if the line is a block header
-            let header_parse_result = parse_block_header(&line, &block_header_regex);
+            let header_parse_result = block_header_parser.parse_block_header(&line);
             if let Some(header_info) = header_parse_result{
                 //if so, should initialize a new block
                 current_block = Some(StructureBlock { 
@@ -149,7 +164,7 @@ pub mod arc_parser{
                 current_block = None;
             }
             //check if the line is a atom information line
-            let atom_parse_result = parse_atom_data(&line, &atom_data_regex);
+            let atom_parse_result = atom_data_parser.parse_atom_data(&line);
             if let Some(atom_info) = atom_parse_result{
                 //if so, should add the atom to the current block
                 //initialize new atom
@@ -163,7 +178,7 @@ pub mod arc_parser{
                 }
             }
             //check if the line is a cell information line
-            let crystal_parse_result = parse_crystal_data(&line, &crystal_info_regex);
+            let crystal_parse_result = cell_data_parser.parse_cell_info(&line);
             //if so, should set the block's crystal info
             if let Some(crystal) = crystal_parse_result{
                 if let Some(block) = current_block.as_mut(){
